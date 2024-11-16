987f15b7fcb2fcd1feefa7f1a32878c4dc5c5756
Jeremias ramirez <Jereramirez499@gmail.com>
2024-11-16 11:12:03 -0400

    creating component 404

---
+++ src/app/Components/skeleton-item/skeleton-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './skeleton-item.component.html',
+  styleUrl: './skeleton-item.component.css'
+})
+export class SkeletonItemComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { ContactsComponent } from './Components/contacts/contacts.component';
+import { NotFound404Component } from './Components/not-found404/not-found404.component';
 
 export const routes: Routes = [
     {
         path: 'contact',
         component: ContactsComponent
+    },
+    {
+        path: '',
+        redirectTo:'/contact',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: NotFound404Component
     }
 ];
