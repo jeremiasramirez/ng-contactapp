8a85b4d6b40d5094e79ccda49860e44089536ad2
Jeremias ramirez <Jereramirez499@gmail.com>
2024-11-16 10:58:02 -0400

    creating main folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ContactsComponent } from './Components/contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'contact',
+        component: ContactsComponent
+    }
+];
